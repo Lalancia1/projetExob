1b03ca822e6c607bf9ed5e285ed380d6a084ae80
Lalancia1 <56025086+Lalancia1@users.noreply.github.com>
2020-01-05 06:17:16 +0100

    Add files via upload

---
+++ exercice1.js
@@ -0,0 +1,17 @@
+var maths=$('#math-heading').text();//on stocke le text dans une variable, pour permettre de compléter via les paramètres
+maths=maths+'...wow!';
+$('#math-heading').text(maths);
+//alert(maths);
+var sciences=$('#science-heading').text;//selection en jquery
+alert(sciences);
+sciences= sciences+'ooooh!!la la';
+$('#science-heading').text(sciences);
+
+
+
+
+
+
+
+
+
